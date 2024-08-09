b1fa614066ec54f621b02acd0a69a2c70a4549c8
cmd05 <splendid.snippet670@slmail.me>
2024-08-09 13:49:06 +0530

    6.4 keyword counter

---
+++ chap06/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct test {char* str; int x};
+
+int main() {
+    // struct test *p;
+    // p->str = "Hello World";
+    // *p->str; // deref `str`
+    // *p->str++; // deref str at the position, then increment `str` (post increment)
+    // *p++->str; // deref str, then increment p
+    // *++p->str; // increment str then deref it
+    // *(++p)->str; // increment p, then deref it's str
+    // (*p->str)++; // increment value (char) at `str`
+    // ++p->str; // pre-increment `str`
+    // p->str++; // post-increment `str`
+    // p++->str; // post increment `p`
+    // ++p->str; // pre increment `p`
+
+    
+}
